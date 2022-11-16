8ec9153f414d718f5daf4f66ddd0416fa8b01719
billyandryus <billyandrys@gmail.com>
2022-11-16 14:46:42 -0500

    delete player react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import background from './assets/img/fondo-PI.jpg'
 import imagenLanding from './assets/img/cute-little-dog-impersonating-business-person.jpg'
 import video from './assets/Cachorro - 4740.mp4'
-import ReactPlayer from 'react-player'
+
 function App() {
   return (
     <Link to='/home'>
