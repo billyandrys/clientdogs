32a12a83b7e8f7952d9cc82d43de4a96c12935bf
billyandryus <billyandrys@gmail.com>
2022-11-28 13:46:43 -0500

    services endpoint

--- src/services/index.js
+++ src/services/index.js
@@ -1,24 +1,18 @@
-/**
+
 export const servicesAllDogs = 'https://apidogs-production.up.railway.app/dogs'
 export const servicesAllTemperaments = 'https://apidogs-production.up.railway.app/allTemperaments'
 export const servicesAllWeight = 'https://apidogs-production.up.railway.app/allWeight'
 export const servicesSearch = 'https://apidogs-production.up.railway.app/dogs?name='
 export const servicesById = 'https://apidogs-production.up.railway.app/dogs/'
 export const servicesCreateDog = 'https://apidogs-production.up.railway.app/dogs/'
-* 
- * 
- * 
- * 
- * 
- * 
- * / */
 
 
 
-export const servicesAllDogs = 'http://localhost:3001/dogs'
-export const servicesAllTemperaments = 'http://localhost:3001/allTemperaments'
-export const servicesAllWeight = 'http://localhost:3001/allWeight'
-export const servicesSearch = 'http://localhost:3001/dogs?name='
-export const servicesById = 'http://localhost:3001/dogs/'
-export const servicesCreateDog = 'http://localhost:3001/dogs'
+
+// export const servicesAllDogs = 'http://localhost:3001/dogs'
+// export const servicesAllTemperaments = 'http://localhost:3001/allTemperaments'
+// export const servicesAllWeight = 'http://localhost:3001/allWeight'
+// export const servicesSearch = 'http://localhost:3001/dogs?name='
+// export const servicesById = 'http://localhost:3001/dogs/'
+// export const servicesCreateDog = 'http://localhost:3001/dogs'
  
